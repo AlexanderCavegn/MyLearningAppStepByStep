f802c843084179e2e249c3a4b43ce5fab859406e
Alexander <cavegn.a@gmx.ch>
2021-08-18 15:31:49 +0200

    Initial Commit

---
+++ myLearningAppStepByStep/myLearningAppStepByStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  myLearningAppStepByStepApp.swift
+//  myLearningAppStepByStep
+//
+//  Created by Alexander on 18.08.21.
+//
+
+import SwiftUI
+
+@main
+struct myLearningAppStepByStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
